d10c32c5142e2ad5e503088d4fa2a2a506ce691d
AElomda <elomdaa7444@gmail.com>
2024-12-09 23:31:55 +0200

    Category CRUD - Edit -Delete

--- ElectroSphere.Web/Controllers/CategoryController.cs
+++ ElectroSphere.Web/Controllers/CategoryController.cs
@@ -23,9 +23,59 @@ namespace ElectroSphere.Web.Controllers
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
-            _context.Categories.Add(category);
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null | id == 0)
+            {
+                NotFound();
+            }
+            var categoryIndb = _context.Categories.Find(id);
+            return View(categoryIndb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null | id == 0)
+            {
+                NotFound();
+            }
+            var categoryIndb = _context.Categories.Find(id);
+            return View(categoryIndb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(int? id)
+        {
+            var categoryIndb = _context.Categories.Find(id);
+            if(categoryIndb == null)
+            {
+                NotFound();
+            }
+            _context.Categories.Remove(categoryIndb);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
